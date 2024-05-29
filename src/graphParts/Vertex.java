a145578d2bdd774e965613cb71fe47b184ff7bb3
McNester <seraf5@bk.ru>
2024-05-29 22:19:47 +0500

    created Vertex and Edge class with corresponding methods

---
+++ src/graphParts/Vertex.java
@@ -0,0 +1,24 @@
+package graphParts;
+
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+}
